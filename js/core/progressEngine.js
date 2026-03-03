ecb3ba32b537f9f1dc5fa6b22cfd3d9f034f80e9
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-03 17:16:55 +0530

    Update progressEngine.js

--- js/core/progressEngine.js
+++ js/core/progressEngine.js
@@ -1,1 +1,10 @@
-// Progress Bar Controller
+export function startProgress() {
+    document.getElementById("progress-bar").style.width = "60%";
+}
+
+export function finishProgress() {
+    document.getElementById("progress-bar").style.width = "100%";
+    setTimeout(() => {
+        document.getElementById("progress-bar").style.width = "0%";
+    }, 400);
+}
