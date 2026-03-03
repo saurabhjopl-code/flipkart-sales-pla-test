f7cd83911ee0e84a57a296df18ec1b6ee7549fca
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-03 17:16:00 +0530

    Update stateManager.js

--- js/core/stateManager.js
+++ js/core/stateManager.js
@@ -1,1 +1,29 @@
-// Global State Manager
+export const STATE = {
+    rawData: {
+        CDR: [],
+        CFR: [],
+        PPR: [],
+        CKR: [],
+        GMV: [],
+        CTR: []
+    },
+    filters: {
+        acc: [],
+        startDate: null,
+        endDate: null
+    },
+    listeners: []
+};
+
+export function setFilters(newFilters) {
+    STATE.filters = { ...STATE.filters, ...newFilters };
+    notify();
+}
+
+export function subscribe(listener) {
+    STATE.listeners.push(listener);
+}
+
+function notify() {
+    STATE.listeners.forEach(fn => fn());
+}
