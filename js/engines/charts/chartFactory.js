4612fdc6984b3a5584b6af53cd167f7d907f1a7a
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-03 17:57:57 +0530

    Update chartFactory.js

--- js/engines/charts/chartFactory.js
+++ js/engines/charts/chartFactory.js
@@ -1,1 +1,23 @@
-// Chart Type Factory
+export function renderLineChart(canvasId, labels, datasets) {
+
+    const ctx = document.getElementById(canvasId).getContext("2d");
+
+    new Chart(ctx, {
+        type: "line",
+        data: {
+            labels,
+            datasets
+        },
+        options: {
+            responsive: true,
+            interaction: { mode: "index", intersect: false },
+            plugins: {
+                legend: { position: "top" }
+            },
+            scales: {
+                x: { display: true },
+                y: { display: true }
+            }
+        }
+    });
+}
