a3fcff005a72bbecf9b497dea10b519f7f9e5d33
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-03 17:22:42 +0530

    Update appBinder.js

--- js/binders/appBinder.js
+++ js/binders/appBinder.js
@@ -3,23 +3,20 @@ import { renderSidebar } from "../renderers/sidebarRenderer.js";
 import { renderFilters } from "../renderers/filterRenderer.js";
 import { loadAllData } from "../core/dataLoader.js";
 import { subscribe } from "../core/stateManager.js";
-
-function renderApp() {
-    document.getElementById("app-content").innerHTML =
-        "<h2>System Ready</h2><p>Reports will render here.</p>";
-}
+import { renderHome } from "./homeBinder.js";
 
 async function init() {
+
     renderHeader();
     renderSidebar();
     renderFilters();
 
     await loadAllData();
 
-    renderApp();
+    renderHome();
 
     subscribe(() => {
-        renderApp();
+        renderHome();
     });
 }
 
