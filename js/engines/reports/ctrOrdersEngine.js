08aa1d0e36ea640433d252423dc8e91d8b0ea797
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-05 16:26:36 +0530

    Update ctrOrdersEngine.js

--- js/engines/reports/ctrOrdersEngine.js
+++ js/engines/reports/ctrOrdersEngine.js
@@ -2,31 +2,46 @@ import { STATE } from "../../core/stateManager.js";
 
 export function getCtrOrders(){
 
-const data =
-STATE?.data?.ctr ||
-STATE?.data?.CTR ||
-STATE?.data?.ctrData ||
-[];
+const rows = STATE.rawData.CTR || [];
+const filters = STATE.filters || {};
 
-const rows = data.map(row => {
+const data = [];
 
+rows.forEach(row => {
+
+const acc = row["ACC"] || "";
 const date = row["Order Date"] || "";
+
+/* ACCOUNT FILTER */
+if(filters.acc && filters.acc !== "All Accounts"){
+if(acc !== filters.acc) return;
+}
+
+/* DATE FILTER */
+if(filters.startDate){
+if(new Date(date.split("/").reverse().join("-")) < new Date(filters.startDate)) return;
+}
+
+if(filters.endDate){
+if(new Date(date.split("/").reverse().join("-")) > new Date(filters.endDate)) return;
+}
+
 const sku = row["SKU"] || "";
 const type = row["Event Type"] || "";
 const fulfilment = row["Fulfilment Type"] || "";
 const price = Number(row["Price before discount"] || 0);
 
-return {
+data.push({
 date,
 sku,
 type,
 fulfilment,
 price
-};
+});
 
 });
 
-rows.sort((a,b)=>{
+data.sort((a,b)=>{
 
 const d1 = new Date(a.date.split("/").reverse().join("-"));
 const d2 = new Date(b.date.split("/").reverse().join("-"));
@@ -35,6 +50,6 @@ return d2 - d1;
 
 });
 
-return rows.slice(0,200);
+return data.slice(0,200);
 
 }
