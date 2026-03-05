5a9b006acd60a4aea69acda54b8abed02ae321f9
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-05 16:42:01 +0530

    Update ctrTrendEngine.js

--- js/engines/reports/ctrTrendEngine.js
+++ js/engines/reports/ctrTrendEngine.js
@@ -3,7 +3,10 @@ import { STATE } from "../../core/stateManager.js";
 export function getCtrTrend(){
 
 const rows = STATE.rawData.CTR || [];
-const filters = STATE.filters || {};
+
+const accFilter = STATE.ui.acc;
+const startDate = STATE.ui.startDate;
+const endDate = STATE.ui.endDate;
 
 const map = {};
 
@@ -15,32 +18,34 @@ const date = r["Order Date"];
 if(!date) return;
 
 /* ACCOUNT FILTER */
-if(filters.account && filters.account !== "All Accounts"){
-if(acc !== filters.account) return;
+
+if(accFilter && accFilter !== "All Accounts"){
+if(acc !== accFilter) return;
 }
 
 /* DATE FILTER */
-if(filters.startDate || filters.endDate){
 
 const d = new Date(date.split("/").reverse().join("-"));
 
-if(filters.startDate){
-const sd = new Date(filters.startDate);
+if(startDate){
+const sd = new Date(startDate);
 if(d < sd) return;
 }
 
-if(filters.endDate){
-const ed = new Date(filters.endDate);
+if(endDate){
+const ed = new Date(endDate);
 if(d > ed) return;
-}
-
 }
 
 const type = (r["Event Type"] || "").trim();
 const value = Number(r["Price before discount"] || 0);
 
 if(!map[date]){
-map[date] = { sales:0, cancel:0, return:0 };
+map[date] = {
+sales:0,
+cancel:0,
+return:0
+};
 }
 
 if(type === "Sale") map[date].sales += value;
@@ -58,21 +63,21 @@ return da - db;
 
 });
 
-const sales=[];
-const cancel=[];
-const ret=[];
-const net=[];
+const sales = [];
+const cancel = [];
+const ret = [];
+const net = [];
 
 labels.forEach(d=>{
 
-const s=map[d].sales;
-const c=map[d].cancel;
-const r=map[d].return;
+const s = map[d].sales;
+const c = map[d].cancel;
+const r = map[d].return;
 
 sales.push(s);
 cancel.push(c);
 ret.push(r);
-net.push(s-c-r);
+net.push(s - c - r);
 
 });
 
